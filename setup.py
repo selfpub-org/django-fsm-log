e162103220fc06fd55e953210f78bd26373518e7
roxt <roxt@mybook.ru>
2020-01-12 01:54:47 +0300

    remove py2 compability

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     url='https://github.com/gizmag/django-fsm-log',
     license='MIT',
     packages=find_packages(),
-    install_requires=['django>=1.8', 'django_fsm>=2', 'django_appconf'],
+    install_requires=['django>=2.0', 'django_fsm>=2', 'django_appconf'],
     extras_require={
         'testing': [
             'pytest',
@@ -32,21 +32,15 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.10',
-        'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
